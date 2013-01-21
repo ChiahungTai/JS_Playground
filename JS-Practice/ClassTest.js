0fe197e7b57688102a380bcffd8a9152cadd09cd
Chia-hung Tai <ctai@mozilla.com>
2013-01-21 19:14:46 +0800

    Add comment on inherit

--- JS-Practice/ClassTest.js
+++ JS-Practice/ClassTest.js
@@ -1,3 +1,6 @@
+// inherit() returns a newly created object that inherits properties from the
+// prototype object p. It uses the ECMAScript 5 function Object.create() if
+// it is defined, and otherwise falls back to an older technique.
 function inherit(p) {
 	if (p == null)
 		throw TypeError(); // p must be a non-null object
